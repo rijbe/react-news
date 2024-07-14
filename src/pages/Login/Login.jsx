c6660647bc1cd87f35e0bfdb5de627420f4b5683
RakibRahmane <rikonrakib12@gmail.com>
2024-07-14 22:11:08 +0600

    add to file

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Shared/Navbar/Navbar';
+
+const Login = () => {
+    return (
+        <div>
+        <Navbar></Navbar>
+            <h1>Login Page</h1>
+        </div>
+    );
+};
+
+export default Login;
